e204fca4cf08b397cb5f3dcf7956a091698916cc
bdubeJHB <bdube@student.wethinkcode.co.za>
2021-01-22 23:49:44 +0200

    Initializing repo with a Maven project

---
+++ src/main/java/com/runaddictmusic/substring/BruteForce.java
@@ -0,0 +1,13 @@
+package com.runaddictmusic.substring;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
